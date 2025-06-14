ca3cc7a3bfbe348d6dff87a8f37a2d3229d063f4
Gabriel Ribeiro <chucrutsgabriel@gmail.com>
2025-06-14 02:54:34 -0300

    feat: leitura .csv e previsao de notas por regressao linear

---
+++ app.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/enviar', methods=['POST'])
+def enviar():
+    file = request.files['csvFile']
+    df = pd.read_csv(file)
+
+    print(df)
+
+    df['Semestre'] = df['Semestre'].astype(int)
+    df['Nota'] = df['Nota'].astype(int)
+
+    x = df['Semestre'].values.reshape(-1, 1)
+    y = df['Nota'].values
+
+    modelo = LinearRegression()
+    modelo.fit(x, y)
+
+    proximo_semestre = x.max() + 1
+    previsao = modelo.predict(np.array([[proximo_semestre]]))[0]
+
+
+    return render_template('previsao.html', proximo_semestre=proximo_semestre, previsao=previsao)
+
+if __name__ == '__main__':
+    app.run(debug=True)
